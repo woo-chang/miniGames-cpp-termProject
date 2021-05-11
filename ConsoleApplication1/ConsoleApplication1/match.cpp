84a6260b47a721a4a404ecb8e73ae45a8fadf769
woo-chang <clllickme@naver.com>
2021-05-12 01:23:39 +0900

    Feat: Update & Add function

--- ConsoleApplication1/ConsoleApplication1/match.cpp
+++ ConsoleApplication1/ConsoleApplication1/match.cpp
@@ -1,6 +1,6 @@
 #include "match.h"
 
-CardGame::CardGame() : reverse_count(0), match_count(0)
+CardGame::CardGame() : reverse_count(0), match_count(0), flip_count(0)
 {
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
@@ -32,33 +32,121 @@ void CardGame::movePos(int x, int y)
 void CardGame::showExplain()
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // 콘솔 창의 글자색을 설정합니다.
-	movePos(30, 2);
+	movePos(30, 5);
 	cout << "<Follow the Sequence of Alphabet>";
-	movePos(29, 3);
-	cout << "☆ A부터 X까지 순서대로 찾아보세요 ☆";
-	movePos(38, 7);
-	cout << "UP";
-	movePos(38, 8);
-	cout << "△";
-	movePos(34, 9);
-	cout << "LEFT ◁    ▷ RIGHT";
+	movePos(29, 6);
+	cout << "☆ A부터 Y까지 순서대로 찾아보세요 ☆";
 	movePos(38, 10);
-	cout << "▽";
+	cout << "UP";
 	movePos(38, 11);
+	cout << "△";
+	movePos(34, 12);
+	cout << "LEFT ◁    ▷ RIGHT";
+	movePos(38, 13);
+	cout << "▽";
+	movePos(38, 14);
 	cout << "DOWN";
-	movePos(35, 14);
+	movePos(35, 17);
 	cout << "CLICK : SPACE";
-	int a;
-	cin >> a;
-	cout << a;
 }
 
 void CardGame::gameStart()
 {
-
+	int keyBoard = 0;
+	while (true) {
+		keyBoard = getInput();
+		if (keyBoard == 224) // 방향키가 입력 되었을 때
+		{
+			changePos();
+		}
+		if (keyBoard == 32) // 스페이스바가 입력 되었을 때
+		{
+			openCard();
+		}
+		showNow();
+		Sleep(100);
+		checkNow();
+	}
 }
 
 int CardGame::getInput()
 {
+	if (_kbhit() != 0)
+	{
+		return _getch();
+	}
 	return 0;
+}
+
+void CardGame::changePos()
+{
+	movePos(1 + (now_pos.x * 3), 1 + (now_pos.y * 2) + 1);
+	printf("  ");
+	switch (_getch())
+	{
+	case 75:
+		--now_pos.x;
+		if (now_pos.x <= 0) now_pos.x = 0;
+		break;
+	case 77:
+		++now_pos.x;
+		if (now_pos.x >= 4) now_pos.x = 4;
+		break;
+	case 72:
+		--now_pos.y;
+		if (now_pos.y <= 0) now_pos.y = 0;
+		break;
+	case 80:
+		++now_pos.y;
+		if (now_pos.y >= 4) now_pos.y = 4;
+		break;
+	}
+}
+
+void CardGame::openCard()
+{
+	if (reverseCard[now_pos.x][now_pos.y] != '#') return;
+	if (flip_count > 1) return;
+	reverse_pos.x = now_pos.x;
+	reverse_pos.y = now_pos.y;
+	reverseCard[now_pos.x][now_pos.y] = gameCard[now_pos.x][now_pos.y];
+	++flip_count;
+}
+
+void CardGame::showNow()
+{
+	char c = '\0';
+	for (int y = 0; y < 5; y++) {
+		movePos(1, 1 + (2 * y));
+		for (int x = 0; x < 5; x++) {
+			c = reverseCard[x][y];
+			printf("[%c]   ", c);
+		}
+	}
+	movePos(1 + (now_pos.x * 3), 1 + (now_pos.y * 2) + 1);
+	printf(" ^");
+	movePos(30, 20);
+	printf("Count : %d", reverse_count);
+}
+
+void CardGame::checkNow()
+{
+	if (flip_count == 1) {
+		char c = '#';
+		if (gameCard[now_pos.x][now_pos.y] == alpha[match_count]) {
+			c = gameCard[now_pos.x][now_pos.y];
+			++match_count;
+		}
+		else {
+			Sleep(500);
+		}
+		reverseCard[now_pos.x][now_pos.y] = c;
+		flip_count = 0;
+		++reverse_count;
+		if (match_count >= 25) {
+			Sleep(500);
+			system("cls");
+			init();
+		}
+	}
 }
--- ConsoleApplication1/ConsoleApplication1/match.h
+++ ConsoleApplication1/ConsoleApplication1/match.h
@@ -10,6 +10,15 @@ struct Pos {
 	int y;
 };
 
+// 게임을 위한 알파벳 2차원 배열 구성입니다.
+const char gameCard[5][5] = { {'A', 'B', 'C', 'D', 'E'},
+							 {'F', 'G', 'H', 'I', 'J'},
+							 {'K', 'L', 'M', 'N', 'O'},
+							 {'P', 'Q', 'R', 'S', 'T'},
+							 {'U', 'V', 'W', 'X', 'Y'} };
+
+const char alpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+
 // Game 진행을 위한 클래스입니다.
 class CardGame
 {
@@ -17,6 +26,7 @@ private:
 	char reverseCard[5][5]; // 게임을 진행할 5X5 보드판을 나타냅니다.
 	Pos now_pos; // 현재 커서의 위치를 저장할 변수입니다.
 	Pos reverse_pos; // 뒤집은 카드 위치를 저장할 변수입니다.
+	int flip_count; // 뒤집은 카드 개수를 저장할 변수입니다.
 	int reverse_count; // 뒤집은 횟수를 저장할 변수입니다.
 	int match_count; // 맞춘 카드 개수를 저장할 변수입니다.
 public:
@@ -26,4 +36,8 @@ public:
 	void showExplain(); // 게임 설명을 콘솔창에 그려주는 함수입니다.
 	void gameStart(); // 본 게임을 시작하는 함수입니다.
 	int getInput(); // 키보드의 입력을 확인하고 이를 반환하는 함수입니다.
+	void changePos(); // 상하좌우 해당 키보드 입력에 따라 위치를 이동하는 함수입니다.
+	void openCard(); // 뒤집혀 있는 카드를 오픈하는 함수입니다.
+	void showNow(); // 현재 상태를 그려주는 함수입니다.
+	void checkNow(); // 현재 진행 상황을 알려주고 게임 진행 여부를 결정하는 함수입니다.
 };
